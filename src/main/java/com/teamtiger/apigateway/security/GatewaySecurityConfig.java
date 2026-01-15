20c2c51420d139489f2cda8d016ba1695a6a7a05
Dan Jackson <danieljackson634@gmail.com>
2026-01-15 16:53:39 +0000

    Added JWT Authentication and Exposed Actuator Endpoints

---
+++ src/main/java/com/teamtiger/apigateway/security/GatewaySecurityConfig.java
@@ -0,0 +1,41 @@
+package com.teamtiger.apigateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Configuration
+public class GatewaySecurityConfig {
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers("/users/login", "/users/register", "/auth/refresh",
+                                "/vendors/login", "/vendors/register", "/actuator/**").permitAll().anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(jwtSpec -> {
+                            jwtSpec.jwtDecoder(reactiveJwtDecoder());
+                        }));
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        return NimbusReactiveJwtDecoder.withSecretKey(new SecretKeySpec(
+                Base64.getDecoder().decode(jwtSecret), "HmacSHA256")).build();
+    }
+
+}
