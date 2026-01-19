a90aac506b60596ae9b9a3581b22104b4cc61217
Dan Jackson <danieljackson634@gmail.com>
2026-01-19 20:42:57 +0000

    Fixed path for Product Service

--- src/main/java/com/teamtiger/apigateway/security/GatewaySecurityConfig.java
+++ src/main/java/com/teamtiger/apigateway/security/GatewaySecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.WebFilter;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
