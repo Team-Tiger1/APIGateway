8e0c485278b0dd5ca376d2af9ffcdeeadea4861b
dann634 <danieljackson634@gmail.com>
2026-03-02 10:41:11 +0000

    Removed old import

--- src/main/java/com/teamtiger/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/teamtiger/apigateway/ApiGatewayApplication.java
@@ -1,6 +1,5 @@
 package com.teamtiger.apigateway;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
